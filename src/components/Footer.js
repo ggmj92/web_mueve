5fbbbf85cffdb9fefc5e3c8b61a32cdb4edd8643
ggmj92 <ggmj92@gmail.com>
2025-09-30 15:16:24 -0500

    footer restructuring for mobile view

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,27 +7,41 @@ export default function Footer() {
                 <div className={styles.col}>
                     <a href="https://maps.app.goo.gl/g22oW8UUtikFqpVe7"
                         target="_blank"
-                        rel="noreferrer">
+                        rel="noreferrer"
+                        className={styles.addressLink}>
                         Gral Borgoño 770, Miraflores
                     </a>
-                    <p>Lima, Perú</p>
+                    <p className={styles.limaPeru}>Lima, Perú</p>
+                    <a href="https://www.instagram.com/mueve.galeria/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className={styles.mobileInstagram}>
+                        INSTAGRAM
+                    </a>
                 </div>
                 <div className={styles.col}>
-                    <a href="mailto:info@mueve.com.pe?subject=Consulta desde Mueve Web&body=Hola,%0D%0A%0D%0AMe gustaría saber más sobre..."
-                        target="_blank"
-                        rel="noreferrer">info@mueve.com.pe
-                    </a>
-                    <br />
                     <a
-                        href="https://wa.me/51987654321?text=Hola,%20estoy%20interesado%20en..."
+                        href="mailto:info@mueve.com.pe?subject=Consulta desde Mueve Web&body=Hola..."
                         target="_blank"
                         rel="noreferrer"
+                        className={styles.contactLine}
+                    >
+                        info@mueve.com.pe
+                    </a>
+                    <a
+                        href="https://wa.me/51987654321?text=Hola..."
+                        target="_blank"
+                        rel="noreferrer"
+                        className={styles.contactLine}
                     >
                         +51 987 654 321
                     </a>
                 </div>
+
                 <div className={styles.col}>
-                    <a href="https://www.instagram.com/mueve.galeria/" target="_blank" rel="noopener noreferrer">
+                    <a href="https://www.instagram.com/mueve.galeria/"
+                        target="_blank"
+                        rel="noopener noreferrer">
                         INSTAGRAM
                     </a>
                 </div>

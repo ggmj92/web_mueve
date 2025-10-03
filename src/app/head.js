e8978859c3447912eccb1080aa480056a83ac279
ggmj92 <ggmj92@gmail.com>
2025-10-03 11:29:20 -0500

    circular pngs added

---
+++ src/app/head.js
@@ -0,0 +1,11 @@
+export default function Head() {
+    return (
+        <>
+            <link rel="icon" href="/logos/favicon.ico" sizes="any" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/logos/favicon-16x16.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/logos/favicon-32x32.png" />
+            <link rel="apple-touch-icon" href="/logos/apple-touch-icon.png" sizes="180x180" />
+            <link rel="icon" type="image/png" sizes="512x512" href="/logos/icon-512.png" />
+        </>
+    )
+}

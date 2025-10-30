b939f598243aa79f9cdfd3d0870ce6ffde293e4a
ggmj92 <ggmj92@gmail.com>
2025-10-30 12:57:33 -0500

    Remove unused urlFor import

--- src/app/artistas/[slug]/page.js
+++ src/app/artistas/[slug]/page.js
@@ -1,5 +1,4 @@
 import { client } from '@/sanity/lib/client'
-import { urlFor } from '@/sanity/lib/image'
 import { PortableText } from '@portabletext/react'
 import NewsletterModal from '@/components/NewsletterModal'
 import styles from './artist.module.css'

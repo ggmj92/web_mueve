563c75386dcc263a8924ffcbfb8c80ecb1bc9bd6
ggmj92 <ggmj92@gmail.com>
2026-05-27 18:20:28 -0500

    feat: register guestArtist schema, update mueveEstar artists to references, add Studio sidebar section

--- src/sanity/schemaTypes/index.js
+++ src/sanity/schemaTypes/index.js
@@ -1,6 +1,7 @@
 import homepage from './homepage'
 import about from './about'
 import artist from './artist'
+import guestArtist from './guestArtist'
 import announcementBanner from './announcementBanner'
 import publicacion from './publicacion'
 import exposicion from './exposicion'
@@ -8,4 +9,4 @@ import feria from './feria'
 import mueveEstar from './mueveEstar'
 // artwork is now inline within artist schema
 
-export const schemaTypes = [homepage, about, artist, announcementBanner, publicacion, exposicion, feria, mueveEstar]
+export const schemaTypes = [homepage, about, artist, guestArtist, announcementBanner, publicacion, exposicion, feria, mueveEstar]

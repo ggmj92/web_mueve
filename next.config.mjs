134665a6e0c882d1f881765873a4d0c70e09a997
ggmj92 <ggmj92@gmail.com>
2026-05-28 09:31:07 -0500

    perf: migrate images.domains to remotePatterns (deprecated API)

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
   
   // Image optimization settings
   images: {
-    domains: ['cdn.sanity.io'],
+    remotePatterns: [{ protocol: 'https', hostname: 'cdn.sanity.io' }],
     formats: ['image/webp', 'image/avif'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
